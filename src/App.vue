861065f51f12cbc19f290a3833edcdfc11cc66bb
浅吟 <7824774+shallow_chant@user.noreply.gitee.com>
2020-12-10 08:37:35 +0800

    初始化项目完成

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div>
+    <h2>黑马头条</h2>
+    <i class="toutiao icon-wuwangluo"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scope></style>
